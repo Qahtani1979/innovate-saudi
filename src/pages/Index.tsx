ae763b16efb79efe7d61192ffd7d133a96ccdd0b
Qahtani1979 <Alqahtani.abdullah@hotmail.com>
2025-12-08 14:29:44 +0300

    Delete src directory

--- src/pages/Index.tsx
+++
@@ -1,9 +0,0 @@
-const Index = () => {
-  return (
-    <div className="min-h-screen flex items-center justify-center">
-      <h1 className="text-2xl font-bold">Ready for import</h1>
-    </div>
-  );
-};
-
-export default Index;
